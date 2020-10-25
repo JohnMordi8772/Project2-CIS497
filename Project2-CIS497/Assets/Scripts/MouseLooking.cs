9dc8471b18fff28de8c72b0033e7b254c223323f
John Mordi <johnmordi00@gmail.com>
2020-10-25 15:11:11 -0500

    Created Testing Scene and PlayerController

---
+++ Project2-CIS497/Assets/Scripts/MouseLooking.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MouseLooking : MonoBehaviour
+{
+    public GameObject player;
+    public float mouseSensitivity = 100f;
+    private float verticalLookRotation = 0f;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float mouseX = Input.GetAxis("Mouse X") * mouseSensitivity * Time.deltaTime;
+        float mouseY = Input.GetAxis("Mouse Y") * mouseSensitivity * Time.deltaTime;
+
+        player.transform.Rotate(Vector3.up * mouseX);
+        verticalLookRotation -= mouseY;
+        verticalLookRotation = Mathf.Clamp(verticalLookRotation, -90f, 90f);
+        transform.localRotation = Quaternion.Euler(verticalLookRotation, 0f, 0f);
+    }
+}
---
+++ Project2-CIS497/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    public CharacterController controller;
+    public float speed = 10f;
+    public float rotationSpeed = 30f;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float x = Input.GetAxis("Horizontal");
+        float z = Input.GetAxis("Vertical");
+
+        Vector3 vector = transform.forward * 5 + transform.right * x;
+        controller.Move(vector * speed * Time.deltaTime);
+    }
+}
