0a9784fb7a77c7ee953fb97fb67d78b4b6555e8f
CaptainVidia <46515050+CaptainVidia@users.noreply.github.com>
2020-10-26 10:49:23 -0500

    OnTriggerEnter Scripts
    allows the player to enter and re enter scenes with trigger areas

---
+++ Project2-CIS497/Assets/Scripts/NextScene.cs
@@ -0,0 +1,24 @@
+﻿/*
+ * Name: George Tang
+ * Project Dream
+ * Purpose: Loads next scene upon portal entry
+ * */
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class NextScene : MonoBehaviour
+{
+    private int nextSceneToLoad;
+    // attach to a portal so player can go back and forth
+    void Start()
+    {
+        nextSceneToLoad = SceneManager.GetActiveScene().buildIndex + 1;
+    }
+
+    private void OnTriggerEnter(Collider collision)
+    {
+        SceneManager.LoadScene(nextSceneToLoad);
+    }
+}
---
+++ Project2-CIS497/Assets/Scripts/PreviousScene.cs
@@ -0,0 +1,27 @@
+﻿/*
+ * Name: George Tang
+ * Project Dream
+ * Purpose: Loads previous scene upon portal entry
+ * */
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+// attach to a portal so player can go back and forth
+public class PreviousScene : MonoBehaviour
+{
+    private int prevSceneToLoad;
+    // Start is called before the first frame update
+    void Start()
+    {
+        // move to previous scene
+        prevSceneToLoad = SceneManager.GetActiveScene().buildIndex - 1;
+
+    }
+
+    private void OnTriggerEnter(Collider collision)
+    {
+        SceneManager.LoadScene(prevSceneToLoad);
+    }
+}
