05a8f6964ca2b77d4d6a8703adac2eda0a17bb59
Josh Bumbalough <jbumbalough@mail.bradley.edu>
2020-11-06 14:20:46 -0600

    Test Portal

--- Project2-CIS497/Assets/Scripts/NextScene.cs
+++ Project2-CIS497/Assets/Scripts/NextScene.cs
@@ -10,12 +10,12 @@ using UnityEngine.SceneManagement;
 
 public class NextScene : MonoBehaviour
 {
-    private int nextSceneToLoad;
+    public int nextSceneToLoad;
     // attach to a portal so player can go back and forth
-    void Start()
-    {
-        nextSceneToLoad = SceneManager.GetActiveScene().buildIndex + 1;
-    }
+    // void Start()
+    // {
+    //     nextSceneToLoad = SceneManager.GetActiveScene().buildIndex + 1;
+    // }
 
     private void OnTriggerEnter(Collider collision)
     {
