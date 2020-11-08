88615c759372a106dddb3f0d1c2c02974f995229
Josh Bumbalough <jbumbalough@mail.bradley.edu>
2020-11-08 15:12:06 -0600

    SoL Scene Changes

---
+++ Project2-CIS497/Assets/Scripts/Collection.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Collection : MonoBehaviour
+{
+    public GameObject hide1;
+    public GameObject hide2;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        hide1 = GameObject.Find("Eagle1");
+        hide2 = GameObject.Find("Eagle2");
+        hide1.gameObject.SetActive(true);
+        hide2.gameObject.SetActive(false);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void OnTriggerEnter(Collider other)
+    {
+        if(other.CompareTag("Player"))
+        {
+            hide1.gameObject.SetActive(false);
+            hide2.gameObject.SetActive(true);
+        }
+    }
+}
